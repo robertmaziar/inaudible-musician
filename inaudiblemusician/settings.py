c8d7637e4675a2ee6e19847ec1c2b5db899d507f
Robert <robert.maziar@gmail.com>
2016-11-27 21:17:41 -0500

    Changed the database.

--- inaudiblemusician/settings.py
+++ inaudiblemusician/settings.py
@@ -77,12 +77,14 @@ WSGI_APPLICATION = 'inaudiblemusician.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'inaudiblemusician',                                   # Or path to database file if using sqlite3.
-        'USER': 'postgres',                                 # Not used with sqlite3.
-        'PASSWORD': 'Amanda2!',                            # Not used with sqlite3.
-        'HOST': 'localhost',                                # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',             
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        #'NAME': 'inaudiblemusician',                                   # Or path to database file if using sqlite3.
+        #'USER': 'postgres',                                 # Not used with sqlite3.
+        #'PASSWORD': 'Amanda2!',                            # Not used with sqlite3.
+        #'HOST': 'localhost',                                # Set to empty string for localhost. Not used with sqlite3.
+        #'PORT': '5432',   
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),          
     }
 }
 

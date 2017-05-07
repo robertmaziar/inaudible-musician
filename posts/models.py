1d559f767a79b649f62ea62dbadb9b06e63eac31
Robert Maziar <robert.maziar@gmail.com>
2017-05-07 00:40:46 -0400

    Repository is out of date. Many changes made.

--- posts/models.py
+++ posts/models.py
@@ -1,20 +1,22 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
-class Post(models.Model): #This line defines the model(object)
-   author = models.ForeignKey('auth.User')
-   title = models.CharField(max_length=200)
-   text = models.TextField()
-   created_date = models.DateTimeField(default=timezone.now)
-   published_date = models.DateTimeField(blank=True, null=True)
+class Post(models.Model):  # This line defines the model(object)
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
-   def publish(self):
-       self.published_date = timezone.now()
-       self.save()
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
-   def __str__(self): # this is the function/method
-       return self.title
+    def __str__(self):  # this is the function/method
+        return self.title
+
 
 class Comment(models.Model):
     post = models.ForeignKey('posts.Post', related_name='comments')
@@ -24,11 +26,11 @@ class Comment(models.Model):
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
-        self.approved_comment =True
+        self.approved_comment = True
         self.save()
 
     def __str__(self):
         return self.text
 
-    #def approved_comments(self):
-     #   return self.comments.filter(approved_comment=True)
+        # def approved_comments(self):
+        #   return self.comments.filter(approved_comment=True)
--- posts/views.py
+++ posts/views.py
@@ -3,31 +3,53 @@ from django.utils import timezone
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.core.exceptions import PermissionDenied
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    post_qs = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    post_q = request.GET.get("q")
+    if post_q:
+        post_qs = post_qs.filter(
+            Q(title__icontains=post_q) |
+            Q(text__icontains=post_q)
+        ).distinct()
+    paginator = Paginator(post_qs, 10)  # Show #number contacts per page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, 'posts/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'posts/post_detail.html', {'post': post})
 
+
 def post_new(request):
     if request.method == "POST":
-	    form = PostForm(request.POST)
-	    if form.is_valid():
-	        post = form.save(commit=False)
-	        post.author = request.user
-	        post.published_date = timezone.now()
-	        post.save()
-	        return redirect('post_detail', pk=post.pk)
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
 
     else:
         form = PostForm()
     return render(request, 'posts/post_edit.html', {'form': form})
+
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -50,7 +72,7 @@ def post_remove(request, pk):
         post.delete()
         return redirect('post_list')
     else:
-        raise PermissionDenied #Throws Error
+        raise PermissionDenied
 
 
 def add_comment_to_post(request, pk):
@@ -67,6 +89,7 @@ def add_comment_to_post(request, pk):
         form = CommentForm()
     return render(request, 'posts/add_comment_to_post.html', {'form': form})
 
+
 """
 @login_required
 def comment_approve(request, pk):
@@ -74,6 +97,7 @@ def comment_approve(request, pk):
     comment .approve()
     return redirect('post_detail', pk=comment.post.pk)
 """
+
 
 @login_required
 def comment_remove(request, pk):
